6af01170282ca021ef45385f4fe3270c706a1ef6
fitsumtsehay <tsehayfitsum@gmail.com>
2020-04-24 16:50:47 -0400

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,34 @@ add each element to sum.
 Print sum.
 
  */
+import java.util.Scanner;
 public class Main {
-    public static void main(String [] args) {
-int [] ints = {1,2,3,4,5,6,7,8,9,10};
-int sum = 0;
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
 
-/*begin loop*/
+        /*begin loop*/
 
-for ( int num : ints) {
-    sum = sum+num;
+        for (int num : ints) {
+            sum = sum + num;
+        }
+            /* print output*/
+            System.out.println("The total number is " + sum);
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter 10 numbers:");
+            int[] array = new int[10];
+            int i;
+            int sum2=0;
+            for (i = 0; i < 10; i += 1) {
+                array[i] = input.nextInt();
 
-/* print output*/
-System.out.println("The total number is " +sum);
-    }
-}}
+                sum2 = sum2 + array[i];
+
+        }
+            System.out.println(" The total sum is: " + sum2);
+
+
+        System.out.println("the average is "+sum2/10);
+
+
+    }}
